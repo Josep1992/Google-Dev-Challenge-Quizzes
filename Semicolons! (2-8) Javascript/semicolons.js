c5b15ac622c5c9f2729b6fac59a3048efee04315
Josep <coding1992@outlook.com>
2018-01-14 00:15:30 -0400

    Added semicolons quiz

---
+++ Semicolons! (2-8) Javascript/semicolons.js
@@ -0,0 +1,15 @@
+// Directions:
+// Define two variables called thingOne and thingTwo and assign them values. Print the values of both variables in one console.log statement using concatenation. For example,
+
+// red blue
+// where "red" is the value of thingOne and "blue" is the value of thingTwo. Don't forget to use semicolons!
+
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ */
+
+// your code goes here
+var thingOne = 5;
+var thingTwo = 10;
+
+console.log(thingOne + " " + thingTwo);
