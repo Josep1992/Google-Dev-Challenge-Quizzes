8424ee95def4d5bbb6dd8c476a1de0e0c26fe2bf
Josep <coding1992@outlook.com>
2018-01-14 00:18:42 -0400

    Added My Name quiz

---
+++ What's my Name (2-9) Javascript/myName.js
@@ -0,0 +1,11 @@
+// Directions:
+// Create a variable called fullName and assign it your full name as a string.
+
+
+/*
+ * Programming Quiz: What's my Name? (2-9)
+ */
+
+// your code goes here
+
+var fullName = "Josep Lopez Hernandez";
