41bfc08330fcb9e976aa37a19d1fed0e7853162b
Josep <coding1992@outlook.com>
2018-01-13 20:15:13 -0400

    added Js quizzes

---
+++ Converting Tempatures (2-2)/temperature.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Converting Tempatures (2-2)
+ *
+ * Use the Celsius-to-Fahrenheit formula to set the fahrenheit varible:
+ *
+ *     F = C x 1.8 + 32
+ *
+ * Log the fahrenheit variable to the console.
+ *
+ */
+
+var celsius = 12;
+var fahrenheit = celsius * 1.8 + 32
+
+console.log(fahrenheit);
