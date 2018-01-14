b17c43ff9e0619ad3ccf8891af73b78102d32106
Josep <coding1992@outlook.com>
2018-01-14 01:13:33 -0400

    Added awesome message quiz

---
+++ One Awesome Message (2-12) Javascript/message.js
@@ -0,0 +1,36 @@
+// Directions:
+// Here are two awesome messages:
+
+// "Hi, my name is Julia. I love cats. In my spare time, I like to play video games."
+// "Hi, my name is James. I love baseball. In my spare time, I like to read."
+// Declare and assign values to three variables for each part of the sentence that changes (firstName, interest, and hobby).
+
+// Use your variables and string concatenation to create your own awesome message and store it in an awesomeMessage variable. Finally, print your awesome message to the JavaScript console.
+
+/*
+ * Programming Quiz: One Awesome Message (2-12)
+ *
+ * 1. Create the variables:
+ *     - firstName
+ *     - interest
+ *     - hobby
+ * 2. Create a variable named awesomeMessage and, using string concatenation 
+      and the variables above, create an awesome message.
+ * 3. Print the awesomeMessage variable to the console.
+ */
+
+/*
+ * Notes:
+ * - Using the above as an example, firstName would have been assigned to
+ *   "Julia", interest to "cats", and hobby to "play video games".
+ * - Be sure to include spaces and periods where necessary!
+ */
+
+// Add your code here
+
+var firstName = "Julia";
+var interest = "Cats";
+var hobby = "to play video games";
+
+var awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest + ". In my spare time, I like to " + hobby + "."
+console.log(awesomeMessage);
