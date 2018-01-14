6afa574e70bb2f3dd4549fd0e059476da2ef5281
Josep <coding1992@outlook.com>
2018-01-13 23:48:17 -0400

    Added another js quiz

---
+++ All Tied Up (2-5) Javascript/tiedUp.js
@@ -0,0 +1,17 @@
+// Directions:
+// Build a single string that resembles the following joke.
+
+// Why couldn't the shoes go out and play?
+// They were all "tied" up!
+
+// Your joke should take the format of a question and answer. The first line should be a question and the second line should be an answer.
+
+// Hint: You will need to use special characters to produce the following output.
+
+
+/*
+ * Programming Quiz: All Tied Up (2-5)
+ */
+
+var joke = "Why coudn't the shoes go out and play? \n They were all 'tied' up!";
+console.log(joke);
