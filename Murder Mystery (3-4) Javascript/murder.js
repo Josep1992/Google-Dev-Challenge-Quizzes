e829f4214265a9156856d214f330727fa8314ffb
Josep <coding1992@outlook.com>
2018-01-26 08:41:15 -0400

    Added Murder Quiz

--- Murder Mystery (3-4) Javascript/murder.js
+++ Murder Mystery (3-4) Javascript/murder.js
@@ -37,22 +37,34 @@
  */
 
 // change the value of `room` and `suspect` to test your code
-var room = "dining room";
-var suspect = "Mr. Parkes";
+// change the value of `room` and `suspect` to test your code
+var room = "gallery";
+var suspect = "Ms. Van Cleve";
 
 var weapon = "";
 var solved = false;
 
-if (/* your conditional goes here */) {
-    
-} else if (/* your conditional goes here */) {
-    
-} else if (/* your conditional goes here */) {
-    
+if (room === "gallery") {
+	weapon = "trophy";
+	if (suspect === "Ms. Van Cleve") {
+		solved = true;
+	}
+} else if (room === "ballroom") {
+	weapon = "poison";
+	if (suspect === "Mr. Kalehoff") {
+		solved = true;
+	}
+} else if (room === "billiards room") {
+	weapon = "pool stick";
+	if (suspect === "Mrs. Sparr") {
+		solved = true;
+	}
 } else {
-    
+	weapon = "knife";
+	if (suspect === "Mr. Parkes") {
+		solved = true;
+	}
 }
-
 if (solved) {
-	console.log(/* your message goes here*/);
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
 }
