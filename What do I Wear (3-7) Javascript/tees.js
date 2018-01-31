3a26b9d31f693c067ddadb9bd4e82507c4524d96
Josep <coding1992@outlook.com>
2018-01-30 21:20:22 -0400

    Added Tees quiz

---
+++ What do I Wear (3-7) Javascript/tees.js
@@ -0,0 +1,37 @@
+// Directions:
+// Use the sizing chart above, create a series of logical expressions that prints the size of a t-shirt based on the measurements of shirtWidth, shirtLength, and shirtSleeve. Valid sizes include S, M, L, XL, 2XL, and 3XL.
+
+// For example, if...
+
+// var shirtWidth = 23; // size L (large)
+// var shirtLength = 30; // size L (large)
+// var shirtSleeve = 8.71; // size L (large)
+// Then print L to the console.
+
+// Hint: You will need to compare a range of values when checking for shirtWidth, shirtLength, and shirtSleeve. For example, if the shirt's width is at least 20", but no more than 22", then the t-shirt should be medium (M) — as long as the other values for the shirt's length and sleeve measurements match up.
+
+// If shirtWidth, shirtLength, and shirtSleeve don't fit within the range of acceptable values for a specific size, then print N/A to the console. For example, if...
+
+// var shirtWidth = 18; // size S (small)
+// var shirtLength = 29; // size M (medium)
+// var shirtSleeve = 8.47; // size M (medium)
+// Then print N/A to the console because the measurements don't all match up with one particular size.
+
+// TIP: Make sure to test your code with different values. For example,
+
+// If shirtWidth equals 19, shirtLength equals 28 and shirtSleeve equals 8.21, then S should be printed to the console.
+// If shirtWidth equals 26, shirtLength equals 33 and shirtSleeve equals 9.63, then 2XL should be printed to the console.
+// If shirtWidth equals 18, shirtLength equals 29 and shirtSleeve equals 8.47, then N/A should be printed to the console.
+
+// Your Code:
+
+/*
+ * Programming Quiz: What do I Wear? (3-7)
+ */
+
+// change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
+var shirtWidth = 23;
+var shirtLength = 30;
+var shirtSleeve = 8.71;
+
+// your code goes here
