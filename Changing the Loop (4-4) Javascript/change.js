35b91e8ff66b81bc0d20eb1351ba4faf453ed0fc
Josep <coding1992@outlook.com>
2018-02-01 22:11:08 -0400

    added change the loop quiz

---
+++ Changing the Loop (4-4) Javascript/change.js
@@ -0,0 +1,30 @@
+// Directions:
+//   Rewrite the following
+// while loop as a
+// for loop:
+
+//   var x = 9;
+// while (x >= 1) {
+//   console.log("hello " + x);
+//   x = x - 1;
+// }
+// Your Code:
+
+
+/*
+ * Programming Quiz: Changing the Loop (4-4)
+ */
+
+// rewrite the while loop as a for loop
+
+// var x = 9;
+// while (x >= 1) {
+//   console.log("hello " + x);
+//   x = x - 1;
+// }
+
+
+
+for (var x = 9; x >= 1; x--) {
+  console.log("hello " + x)
+}
