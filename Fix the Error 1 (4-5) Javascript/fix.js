6c1bd028b3831835d655575447903c117633e4da
Josep <coding1992@outlook.com>
2018-02-01 22:30:56 -0400

    Added error quizzes

---
+++ Fix the Error 1 (4-5) Javascript/fix.js
@@ -0,0 +1,21 @@
+// Directions:
+// Here is a for loop that's supposed to print the numbers 5 through 9. Fix the error!
+
+// for (x < 10; x++) {
+//   console.log(x);
+// }
+
+// /*
+//  * Programming Quiz: Fix the Error 1 (4-5)
+//  */
+
+
+//   fix the for loop
+
+// for (x < 10; x++) {
+//   console.log(x);
+// }
+
+for (var x = 5; x < 10; x++) {
+  console.log(x);
+}
---
+++ Fix the Error 2 (4-6) Javascript/error2.js
@@ -0,0 +1,24 @@
+// Directions:
+//   The
+// for loop below has an error.Fix it!
+
+//   for (var k = 0 k < 200 k++) {
+//     console.log(k);
+//   }
+// Your Code:
+
+/*
+ * Programming Quiz: Fix the Error 2 (4-6)
+ */
+
+// fix the for loop
+// for (var k = 0 k < 200 k++) {
+//   console.log(k);
+// }
+
+for (var k = 0; k < 200;k++) {
+  console.log(k);
+}
+
+
+
