ba6b8888860f0c5fb765089a8395a1ab522d3f81
Josep <coding1992@outlook.com>
2018-02-02 23:12:34 -0400

    laugh quiz

---
+++ Laugh it Off 1 (5-1) Javascript Functions/laugh.js
@@ -0,0 +1,31 @@
+// Directions:
+// Declare a function called laugh() that returns "hahahahahahahahahaha!". Print the value returned from the laugh() function to the console.
+
+// Your Code:
+
+
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+// your code goes here
+
+function laugh() {
+  return ("hahahahahahahahahaha!");
+}
+
+console.log(laugh());
+
+//or
+
+
+const laugh = () => {
+  return "hahahahahahahahahaha!"
+}
+
+//or
+
+
+function laugh(str) {
+  return str;
+}
