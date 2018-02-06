7c41bff68359edf80da30c31553772a20322c59f
Josep <coding1992@outlook.com>
2018-02-05 22:26:53 -0400

    Prices Quiz

---
+++ The Price is Right (6-3) Javascritp Array's/price.js
@@ -0,0 +1,23 @@
+// Directions:
+// Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
+
+// var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+// TIP: The 1st element of any array has an index of 0.
+
+// Afterwards, print out the prices array to the console.
+
+// Your Code:
+
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
+
+prices[0] = 1.25;
+prices[3] = 7.97;
+prices[7] = 9.99;
+
+console.log(prices);
