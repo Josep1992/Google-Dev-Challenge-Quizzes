9c0e5b8e8e1fed70e081beec67c4aea889e95c05
Josep <coding1992@outlook.com>
2018-02-08 06:49:30 -0400

    Added crew quiz

---
+++ Joining the Crew (6-6) Javascript Array's/crew.js
@@ -0,0 +1,41 @@
+// Directions:
+// In an earlier exercise, you created a crew array to represent Mal’s crew from the hit show Firefly.
+
+// var captain = "Mal";
+// var second = "Zoe";
+// var pilot = "Wash";
+// var companion = "Inara";
+// var mercenary = "Jayne";
+// var mechanic = "Kaylee";
+
+// var crew = [captain, second, pilot, companion, mercenary, mechanic];
+// Later in the show, Mal takes on three new crew members named "Simon", "River", and "Book". Use the push() method to add the three new crew members to the crew array.
+
+// var doctor = "Simon";
+// var sister = "River";
+// var shepherd = "Book";
+
+
+
+/*
+ * Programming Quiz: Joining the Crew (6-6)
+ */
+
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+var crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+var doctor = "Simon";
+var sister = "River";
+var shepherd = "Book";
+
+// your code goes here
+
+crew.push(doctor,sister,shepherd);
+
+console.log(crew);
