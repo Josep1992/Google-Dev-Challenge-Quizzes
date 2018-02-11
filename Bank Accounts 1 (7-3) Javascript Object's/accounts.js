c516a07cfaae6039949c8409c3cd20615e4e52c4
Josep <coding1992@outlook.com>
2018-02-11 14:19:03 -0400

    Accounts

--- Bank Accounts 1 (7-3) Javascript Object's/accounts.js
+++ Bank Accounts 1 (7-3) Javascript Object's/accounts.js
@@ -27,22 +27,24 @@
  * Programming Quiz: Bank Accounts 1 (7-3) Javascript Object's
  */
 
+
+
 let savingsAccount = {
   balance: 1000,
   interestRatePercent: 1,
   deposit: function addMoney(amount) {
-      if (amount > 0) {
-          savingsAccount.balance += amount;
-      }
+    if (amount > 0) {
+      savingsAccount.balance += amount;
+    }
   },
   withdraw: function removeMoney(amount) {
-      var verifyBalance = savingsAccount.balance - amount;
-      if (amount > 0 && verifyBalance >= 0) {
-          savingsAccount.balance -= amount;
-      }
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
   },
-  printAccountSummary: function(){
-      return ("Welcome!/n Your balance is currently $1000 and your interest rate is 1%.");
+  printAccountSummary() {
+    return ("Welcome!\nYour balance is currently $" + savingsAccount.balance + " and your interest rate is " + savingsAccount.interestRatePercent + "%.");
   }
 };
 
