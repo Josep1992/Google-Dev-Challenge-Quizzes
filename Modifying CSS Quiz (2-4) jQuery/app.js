c0da6befea7feb8e6f8d40b6fa4d15baa4c3de85
Josep <coding1992@outlook.com>
2018-02-19 12:36:04 -0400

    Added Modifying CSS Quiz

---
+++ Modifying CSS Quiz (2-4) jQuery/app.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, change the font-size of all the article-items to 20px!
+
+You must use jQuery's css() method!
+*/
+
+// Start with this variable!
+let articleItems;
+
+articleItems = $('.article-item').css("font-size", "20px");
