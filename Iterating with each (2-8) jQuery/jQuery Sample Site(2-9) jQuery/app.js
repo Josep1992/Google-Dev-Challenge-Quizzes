e2359ca8497cbd256582258839550a0f34a21177
Josep <coding1992@outlook.com>
2018-02-19 17:12:14 -0400

    Added jQuery Sample Site Quiz

---
+++ Iterating with each (2-8) jQuery/jQuery Sample Site(2-9) jQuery/app.js
@@ -0,0 +1,20 @@
+/*
+
+For this quiz, can you use this script, which is linked in the <head> of index.html,
+to change the boring placeholder image to a picture of a cute animal?
+
+Remember, you'll need to pass a function into the jQuery object to run
+when the document is ready.
+
+Unfortunately, placepuppy is no longer available. Here's a link to a random
+animal image on lorempixel.com:
+http://lorempixel.com/350/150/animals/
+
+Good luck!
+
+*/
+$('document').ready(function () {
+
+  $('img').attr("src", "http://placekitten.com/350/150");
+
+});
