b5016ca42da5ff2c10494b4f983d86ae3be5b4f0
Josep <coding1992@outlook.com>
2018-02-19 13:21:55 -0400

    Added Removing DOM Elements

---
+++ Removing DOM Elements (2-6) jQuery/app.js
@@ -0,0 +1,11 @@
+/*
+For this quiz, remove the <ul> from the first article item!
+
+You must use jQuery's remove() method.
+*/
+
+// Start with this variable!
+let articleItems;
+
+articleItems = $('.articles .article-item ul').remove();
+
