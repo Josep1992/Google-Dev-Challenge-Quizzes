9236c26d11bee794fbfee7dda8177a5b6a143ad9
Josep <coding1992@outlook.com>
2018-02-19 12:16:06 -0400

    Added changing attribute Quiz

---
+++ Changing Attributes Quiz (2-3) jQuery/app.js
@@ -0,0 +1,12 @@
+/*
+For this quiz, set the href of the <a> in the first nav item to "#1".
+
+You must use jQuery's attr() method!
+*/
+
+// Start with this variable!
+let navList;
+
+navList = $('.nav-item a').first().attr("href","#1");
+
+console.log(navList);
